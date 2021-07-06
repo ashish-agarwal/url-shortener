12d465c3a04cf80c708eaf4596431434a73fac95
Ashish <ashishcd94@gmail.com>
2021-07-06 19:18:29 +0530

    chore: create a API to shorten url

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const shortid = require('shortid');
 
+
+const baseUrl = 'http://localhost:3000/';
+const urlCodes = {};
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  return res.send('Hello');
+
+  const longUrl = req.query.url || req.body.url;
+
+  if (!longUrl) {
+    return res.send({ message: 'No url found to shorten' });
+  } else if (urlCodes[longUrl]) {
+    return res.send({ result: `${baseUrl}/${urlCodes[longUrl]}` });
+  }
+
+  const urlCode = shortid.generate();
+
+  urlCodes[longUrl] = urlCode;
+
+  return res.send({ result: `${baseUrl}/${urlCodes[longUrl]}` });
 });
 
 module.exports = router;
